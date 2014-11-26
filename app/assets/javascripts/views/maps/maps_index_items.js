ca793557690fd36f1489fe0b6964d3eba32157ea
mhepfer <mariehepfer@gmail.com>
2014-11-26 13:54:31 -0800

    more reorganizing, placeMarkers is utilizing a template now

--- app/assets/javascripts/views/maps/maps_index_items.js
+++ app/assets/javascripts/views/maps/maps_index_items.js
@@ -21,13 +21,11 @@ Superhero.Views.MapsIndexItems = Backbone.View.extend({
   },
 
   placeMarkers: function() {
-  	// this.createWindowContent();
-  	// this.infoWindow = new google.maps.InfoWindow({ content: this.contentString });
 
   // Loop through our array of markers & place each one on the map  
   	for( i = 0; i < this.markers.length; i++ ) {
       var position = new google.maps.LatLng(this.markers[i][1], this.markers[i][2]);
-        marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
           position: position,
           map: this.map,
           title: this.markers[i][0]
@@ -41,21 +39,10 @@ Superhero.Views.MapsIndexItems = Backbone.View.extend({
   		}
  	},
 
- 	createWindowContent: function( marker, i ) {
- 		var content = this.template({ marker: marker, counter: i })
+ 	createWindowContent: function( marker ) {
+ 		var content = this.template({ marker: marker })
  		this.infoWindow = new google.maps.InfoWindow({ content: content });
  	},
-
- 	// createWindowContent: function() {
- 	// 	this.contentString ='<div class="info_content">' +
-  //     '<h3>London Eye</h3>' +
-  //     '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' + '</div>'
-  //     '<div class="info_content">' +
-  //     '<h3>Palace of Westminster</h3>' +
-  //     '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-  //     '</div>'
- 	// },
-
 
 
 })
