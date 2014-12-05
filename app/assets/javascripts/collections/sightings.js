ed6b2663004b4c60e6a747437ec64947cf7651d0
mhepfer <mariehepfer@gmail.com>
2014-12-05 14:00:56 -0800

    tried adding a sort by date function to sightings collection. It's not working yet

--- app/assets/javascripts/collections/sightings.js
+++ app/assets/javascripts/collections/sightings.js
@@ -1,6 +1,10 @@
 Superhero.Collections.Sightings = Backbone.Collection.extend({
 	url: "api/sightings",
 	
-  model: Superhero.Models.Sighting
+  model: Superhero.Models.Sighting,
+
+  comparator: function(sighting) {
+  	return -sighting.get('time');
+  }
 
 });
